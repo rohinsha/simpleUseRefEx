4fa4d9790ea0f1369b75e9dbb4ddab83ce3e4a12
rohinsha <r2hinsha@gmail.com>
2023-03-24 01:55:57 +0530

    Change useRef default value to null from 0 as UR.current.value was not working.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import "./styles.css";
 import { useRef, useEffect } from "react";
 export default function App() {
-  const UR = useRef(0);
+  const UR = useRef(null);
   useEffect(() => {
-    UR.current = 3;
+    UR.current.value = 3;
+    UR.current.focus();
   }, []);
   console.log(UR);
   return (
     <div className="App">
+      <input ref={UR} />
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <h1>{UR.current}</h1>
     </div>
   );
 }
